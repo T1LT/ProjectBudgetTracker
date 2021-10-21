a9715090b70c723f635998c58804806c7a17ee02
bhavesh-kankani <bhaveshkankani769@gmail.com>
2021-10-22 00:44:39 +0530

    reports api integration

--- frontend/src/Components/Reports.jsx
+++ frontend/src/Components/Reports.jsx
@@ -1,21 +1,48 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 import "./Reports.css";
 import { tabContext } from "../index";
+import axios from "axios";
 
 const Reports = () => {
+	const [reportData, setReportData] = useState([]);
 	const { settab, projectId } = useContext(tabContext);
 	useEffect(() => {
 		settab("repo");
-	}, [settab]);
+		axios
+			.get(`http://localhost:8000/api/project/${projectId}/report/`)
+			.then((response) => {
+				let data = [];
+				for (let i = 1; i <= 12; i++) {
+					data.push(response.data[`${i}`]);
+				}
+				setReportData([...data]);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, [settab, projectId]);
 	return (
 		<div id="bar">
 			<Bar
 				data={{
-					labels: ["Jan", "Feb", "Mar", "Apr", "May"],
+					labels: [
+						"Jan",
+						"Feb",
+						"Mar",
+						"Apr",
+						"May",
+						"Jun",
+						"Jul",
+						"Aug",
+						"Sep",
+						"Oct",
+						"Nov",
+						"Dec",
+					],
 					datasets: [
 						{
-							data: [1000, 500, 1500, 700, 1300],
+							data: reportData,
 							backgroundColor: [
 								"rgba(255, 99, 132, 0.2)",
 								"rgba(255, 159, 64, 0.2)",
