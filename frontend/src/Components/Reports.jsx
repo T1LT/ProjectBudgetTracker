aee03b9e7d9ef9a5f941b95f0640b80e0f9a334b
Adarsh Reddy <adarshreddy65@gmail.com>
2021-10-29 22:00:36 +0530

    'chart-group'

--- frontend/src/Components/Reports.jsx
+++ frontend/src/Components/Reports.jsx
@@ -19,50 +19,84 @@ const Reports = () => {
         console.log(error);
       });
   }, [settab, projectId]);
+  console.log(reportData);
+  var data = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Param 1",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [1,2,3,4,5,6,7,8,9,10,11,12],
+        borderWidth: 1,
+      },
+      {
+        label: "Param 2",
+        backgroundColor: "rgba(255, 159, 64, 0.2)",
+        borderColor: "rgba(255, 159, 64)",
+        data: [1,2,3,4,5,6,7,8,9,10,11,12],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <div id="outside">
       <div id="bar">
         <Bar
-          data={{
-            labels: [
-              "Jan",
-              "Feb",
-              "Mar",
-              "Apr",
-              "May",
-              "Jun",
-              "Jul",
-              "Aug",
-              "Sep",
-              "Oct",
-              "Nov",
-              "Dec",
-            ],
-            datasets: [
-              {
-                data: reportData,
-                backgroundColor: [
-                  "rgba(255, 99, 132, 0.2)",
-                  "rgba(255, 159, 64, 0.2)",
-                  "rgba(255, 205, 86, 0.2)",
-                  "rgba(75, 192, 192, 0.2)",
-                  "rgba(54, 162, 235, 0.2)",
-                  // "rgba(153, 102, 255, 0.2)",
-                  // "rgba(201, 203, 207, 0.2)",
-                ],
-                borderColor: [
-                  "rgb(255, 99, 132)",
-                  "rgb(255, 159, 64)",
-                  "rgb(255, 205, 86)",
-                  "rgb(75, 192, 192)",
-                  "rgb(54, 162, 235)",
-                  // "rgb(153, 102, 255)",
-                  // "rgb(201, 203, 207)",
-                ],
-                borderWidth: 1,
-              },
-            ],
-          }}
+          // data={{
+          //   labels: [
+          //     "Jan",
+          //     "Feb",
+          //     "Mar",
+          //     "Apr",
+          //     "May",
+          //     "Jun",
+          //     "Jul",
+          //     "Aug",
+          //     "Sep",
+          //     "Oct",
+          //     "Nov",
+          //     "Dec",
+          //   ],
+          //   datasets: [
+          //     {
+          //       data: reportData,
+          //       backgroundColor: [
+          //         "rgba(255, 99, 132, 0.2)",
+          //         "rgba(255, 159, 64, 0.2)",
+          //         "rgba(255, 205, 86, 0.2)",
+          //         "rgba(75, 192, 192, 0.2)",
+          //         "rgba(54, 162, 235, 0.2)",
+          //         // "rgba(153, 102, 255, 0.2)",
+          //         // "rgba(201, 203, 207, 0.2)",
+          //       ],
+          //       borderColor: [
+          //         "rgb(255, 99, 132)",
+          //         "rgb(255, 159, 64)",
+          //         "rgb(255, 205, 86)",
+          //         "rgb(75, 192, 192)",
+          //         "rgb(54, 162, 235)",
+          //         // "rgb(153, 102, 255)",
+          //         // "rgb(201, 203, 207)",
+          //       ],
+          //       borderWidth: 1,
+          //     },
+          //   ],
+          // }}
+          data={data}
           options={{
             scales: {
               x: { beginAtZero: true },
